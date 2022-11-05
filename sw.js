7d5421b5d21110c50e6c2335fe484c73e959e5c3
Karla Dominguez <20183l101008@utcv.edu.mx>
2022-11-05 09:30:16 -0600

    Enviado:D3

--- sw.js
+++ sw.js
@@ -2,17 +2,30 @@ self.addEventListener('install', (event) => {
     //Nosotros deberiamos agregar al cache nuestros archivos
     console.info('[SW]: Adicionando nuestros archivos al cache...');
 
-    setTimeout(() => {
-        console.warn('[SW]: Se instalo correctamente...');
-    }, 1000); //1 segundo
-    self.skipWaiting();
+   const wu = new Promise((resolve, reject) => {
+    try{
+        setTimeout(() => {
+            const adicionandoMisArchivos = '';
+            const adicionarVariablesDinamicas = '';
+            console.warn('[SW]: Se instalo correctamente...');
+            resolve();
+        }, 1000); //1 segundo
+
+        self.skipWaiting();
+    } catch (error){
+        reject(error.message);
+    }
+   })
+   event.waitUntil(wu);
 });
 
 self.addEventListener('activate', (event) => {
+    //La documentacion nos indica eliminar todos los caches atneriores
     console.info('[SW]: Archivos exitosamente guardados...');
     //event.waitUntil(clients.cliam());
 });
 
 self.addEventListener('fetch', (event) => {
     console.info('[SW]: Instalando...');
+    console.log(event.request.url);
 })
